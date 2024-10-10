01a6804948f2eb6eb248750ba52a397d55aef557
dkmos2016 <dkmos2016@126.com>
2024-10-10 14:54:54 +0800

    feat: main适配&更新crate.io依赖

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,70 @@ use parser::{
     base::{BaseParser, BaseReport},
     sast, sca, secret,
 };
-use std::{fs, env};
+// use utils::llm;
+use std::{env, fs};
 use tokio;
 
 pub mod conf;
 pub mod parser;
 pub mod utils;
+// pub mod test;
+
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
+    // llm::init().await;
     // test::test().await;
     // todo!();
     let args: Vec<String> = env::args().collect();
     if let 1 = args.len() {
         log::error!("not enough args");
-        // println!("not enough args");
+        // println!("not enough args");ca
         return;
     }
+    let target = env::var_os("TARGET")
+        .unwrap_or_default()
+        .to_str()
+        .unwrap()
+        .to_owned();
 
     match args[1].as_str() {
-        "sast" => match fs::read_to_string("/works/gl-sast-report.json") {
-            Ok(cont) => {
-                let mut report = sast::SASTReport::new();
-                report.parse(cont.as_str());
-                report.report().await;
+        "sast" => {
+            let fpath = target + "/gl-sast-report.json";
+            match fs::read_to_string(fpath.as_str()) {
+                Ok(cont) => {
+                    let mut report = sast::SASTReport::new();
+                    report.get_diffs().await;
+
+                    report.parse(cont.as_str());
+                    report.report().await;
+                }
+                Err(_e) => log::error!("not such file! {fpath}"),
             }
-            Err(_e) => log::error!("not such file!"),
-        },
-        "sca" => match fs::read_to_string("/works/gl-dependency-scanning-report.json") {
-            Ok(cont) => {
-                let mut report = sca::SCAReport::new();
-                report.parse(cont.as_str());
-                report.report().await;
+        }
+        "sca" => {
+            let fpath = target + "/gl-dependency-scanning-report.json";
+            match fs::read_to_string(fpath.as_str()) {
+                Ok(cont) => {
+                    let mut report = sca::SCAReport::new();
+                    report.parse(cont.as_str());
+                    report.report().await;
+                }
+                Err(_e) => log::error!("not such file! {fpath}"),
             }
-            Err(_e) => log::error!("not such file!"),
-        },
-        "secret" => match fs::read_to_string("/works/gl-secret-detection-report.json") {
-            Ok(cont) => {
-                let mut report = secret::SecretReport::new();
-                report.parse(cont.as_str());
-                report.report().await;
+        }
+        "secret" => {
+            let fpath = target + "/gl-secret-detection-report.json";
+            match fs::read_to_string(fpath.as_str()) {
+                Ok(cont) => {
+                    let mut report = secret::SecretReport::new();
+                    report.parse(cont.as_str());
+                    report.report().await;
+                }
+                Err(_e) => log::error!("not such file! {fpath}"),
             }
-            Err(_e) => log::error!("not such file!"),
-        },
+        }
 
         _default => {
             log::error!("not support");
