19b33de1260bdc43413ce58feebb392ad77a8e9f
dkmos2016 <dkmos2016@126.com>
2024-10-10 14:13:39 +0800

    feat: 移除通用类中的方法定义

--- src/parser/base.rs
+++ src/parser/base.rs
@@ -10,38 +10,7 @@ pub trait BaseParser<T> {
     fn to_issue(self: &Self) -> Issue;
     async fn filter(self: &mut Self) -> Vec<T>;
     async fn is_in_diff(self: &Self, fpath: &str) -> bool {
-        let mut flag = false;
-        // let rt = tokio::runtime::Builder::new_current_thread()
-        // .enable_all()
-        // .build().unwrap();
-
-        // rt.block_on(async  {
-        //     let hash = get_mr_commit_hash(CI_PROJECT_ID.to_owned(), CI_MERGE_REQUEST_IID.to_owned()).await.unwrap();
-        //     let diff_files = get_commit_diff(CI_PROJECT_ID.to_owned(), hash.as_str()).await.unwrap();
-        //     for (_fpath, _) in diff_files.iter() {
-        //         println!("_path: {}, path: {}", _fpath, fpath);
-        //         if _fpath == fpath {
-        //             flag = true;
-        //             break;
-        //         }
-        //     }
-        //     flag
-        // })
-
-        let hash = get_mr_commit_hash(CI_PROJECT_ID.to_owned(), CI_MERGE_REQUEST_IID.to_owned())
-            .await
-            .unwrap();
-        let diff_files = get_commit_diff(CI_PROJECT_ID.to_owned(), hash.as_str())
-            .await
-            .unwrap();
-        for (_fpath, _) in diff_files.iter() {
-            println!("_path: {}, path: {}", _fpath, fpath);
-            if _fpath == fpath {
-                flag = true;
-                break;
-            }
-        }
-        flag
+        false
     }
 
     fn is_block_user() -> bool {
